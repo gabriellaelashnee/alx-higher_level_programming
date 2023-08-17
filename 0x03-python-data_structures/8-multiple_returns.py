3f57150b7d1afe6afc8ccd8d887cb2ce9f10104d
gabriellaelashnee <gabriellaelashneenaidoo@gmail.com>
2023-08-17 12:31:28 -0700

    Python - Data Structures: Lists, Tuples

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
-# 8-multiple_returns.py
-
 
 def multiple_returns(sentence):
-    """Returns the length of a string and its first character."""
-    if sentence == "":
+    if len(sentence) == 0:
         return (0, None)
     return (len(sentence), sentence[0])
-
