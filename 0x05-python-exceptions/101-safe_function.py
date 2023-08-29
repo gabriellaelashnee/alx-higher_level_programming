1df95ce9f0241d9ed4fe09f1c07cce9a17f71b54
gabriellaelashnee <gabriellaelashneenaidoo@gmail.com>
2023-08-29 13:12:15 -0700

    Python - Exceptions

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,28 +1,22 @@
+#!/usr/bin/python3
+
+import sys
+
+
 def safe_function(fct, *args):
+    """Executes a function safely.
+
+    Args:
+        fct: The function to execute.
+        args: Arguments for fct.
+
+    Returns:
+        If an error occurs - None.
+        Otherwise - the result of the call to fct.
+    """
     try:
         result = fct(*args)
-        return result
-    except Exception as e:
-        import sys
-        sys.stderr.write("Exception: {}\n".format(e))
-        return None
-
-if __name__ == "__main__":
-    def my_div(a, b):
-        return a / b
-
-    result = safe_function(my_div, 10, 2)
-    print("result of my_div: {}".format(result))
-
-    result = safe_function(my_div, 10, 0)
-    print("result of my_div: {}".format(result))
-
-    def print_list(my_list, length):
-        i = 0
-        while i < length:
-            print(my_list[i])
-            i += 1
-        return length
-
-    result = safe_function(print_list, [1, 2, 3, 4], 10)
-    print("result of print_list: {}".format(result))
+        return (result)
+    except:
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
+        return (None)
