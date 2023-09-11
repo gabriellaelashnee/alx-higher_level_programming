a8dec79ee52e1ac7200695acd866c2435e4672fb
gabriellaelashnee <gabriellaelashneenaidoo@gmail.com>
2023-09-11 12:52:25 -0700

    Python - Test-driven development

--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -1,13 +1,10 @@
-/*
- * File: 102-python.c
- */
-
 #include "Python.h"
 
 /**
  * print_python_string - Prints information about Python strings.
  * @p: A PyObject string object.
  */
+
 void print_python_string(PyObject *p)
 {
 	long int length;
