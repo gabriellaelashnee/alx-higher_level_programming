9d6b63377f0ad6a46c8ee5462f2498f632ec836a
gabriellaelashnee <gabriellaelashneenaidoo@gmail.com>
2023-09-12 13:39:33 -0700

    Python - Input/Output

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""Module adds all arguments to a Python list and save them to a file."""
-
-
+"""Add all arguments to a Python list and save them to a file."""
 import sys
 
 if __name__ == "__main__":
